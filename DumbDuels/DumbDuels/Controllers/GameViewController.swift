a71a46b06d29d768bd580ab44f12306048868fd2
Esmanda Wong <esmanda3w@gmail.com>
2023-03-12 23:28:24 +0800

    set up basic views

---
+++ DumbDuels/DumbDuels/Controllers/GameViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GameViewController.swift
+//  DumbDuels
+//
+//  Created by Esmanda Wong on 11/3/23.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

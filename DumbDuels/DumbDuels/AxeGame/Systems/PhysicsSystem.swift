7c1be83814391453468fb4497e7b5980ac485f28
Bryan Kwok <bryankwokyf@gmail.com>
2023-03-21 00:53:01 +0800

    Tidy up PhysicsSystem

--- DumbDuels/DumbDuels/AxeGame/Systems/PhysicsSystem.swift
+++ DumbDuels/DumbDuels/AxeGame/Systems/PhysicsSystem.swift
@@ -45,13 +45,11 @@ class PhysicsSystem: System {
     }
 
     func apply(impulse: CGVector, to entityId: EntityID) {
-        print("Physics system trying to add impulse \(impulse) to physics component of \(entityId.id)")
         guard let physicsComponent: PhysicsComponent =
                 entityManager.getComponent(ofType: PhysicsComponent.typeId, for: entityId) else {
             return
         }
         physicsComponent.impulse = impulse
-        print("Physics system added impulse \(impulse) to physics component of \(entityId.id)")
     }
 
     func syncToPhysicsEngine() {
@@ -62,14 +60,16 @@ class PhysicsSystem: System {
                 entityManager.remove(componentType: CollidableComponent.typeId, from: entity.id)
                 continue
             }
-            var physicsBody = initializePhysicsBodyFrom(positionComponent: position,
-                                                        rotationComponent: rotation,
-                                                        physicsComponent: physics,
-                                                        collidableComponent: collidable)
-            scene.sync(physicsBody, for: entity.id.id)
+            if let physicsBody = initializePhysicsBodyFrom(positionComponent: position,
+                                                           rotationComponent: rotation,
+                                                           physicsComponent: physics,
+                                                           collidableComponent: collidable) {
+                scene.sync(physicsBody, for: entity.id.id)
+            } else {
+                assertionFailure("Error creating PhysicsBody when syncing to physics engine.")
+            }
             if physics.impulse != .zero {
                 scene.apply(impulse: physics.impulse, to: entity.id.id)
-                print("Physics systems called scene to add impulse \(physics.impulse) to \(entity.id.id)")
                 physics.impulse = .zero
             }
         }
@@ -78,11 +78,14 @@ class PhysicsSystem: System {
     func setUpPhysics() {
         var entityIDPhysicsBodyMap = [EntityID.ID: PhysicsBody]()
         for (entity, position, rotation, physics, collidable) in physics.entityAndComponents {
-            var physicsBody = initializePhysicsBodyFrom(positionComponent: position,
-                                                        rotationComponent: rotation,
-                                                        physicsComponent: physics,
-                                                        collidableComponent: collidable)
-            entityIDPhysicsBodyMap[entity.id.id] = physicsBody
+            if let physicsBody = initializePhysicsBodyFrom(positionComponent: position,
+                                                           rotationComponent: rotation,
+                                                           physicsComponent: physics,
+                                                           collidableComponent: collidable) {
+                entityIDPhysicsBodyMap[entity.id.id] = physicsBody
+            } else {
+                assertionFailure("Error creating PhysicsBody when setting up PhysicsSystem.")
+            }
         }
         self.scene.setup(newBodyIDPhysicsMap: entityIDPhysicsBodyMap)
     }
@@ -90,35 +93,25 @@ class PhysicsSystem: System {
     private func initializePhysicsBodyFrom(positionComponent: PositionComponent,
                                            rotationComponent: RotationComponent,
                                            physicsComponent: PhysicsComponent,
-                                           collidableComponent: CollidableComponent) -> PhysicsBody {
+                                           collidableComponent: CollidableComponent) -> PhysicsBody? {
         let categoryBitMask = CollisionUtils.bitmasks(for: collidableComponent.categories)
         let collisionBitMask = CollisionUtils.collideBitmasks(for: collidableComponent.categories)
         let contactBitMask = CollisionUtils.contactBitmasks(for: collidableComponent.categories)
-        let physicsBody: PhysicsBody = PhysicsBody(position: positionComponent.position,
-                                                   size: physicsComponent.size,
-                                                   radius: physicsComponent.radius,
-                                                   zRotation: rotationComponent.angleInRadians,
-                                                   mass: physicsComponent.mass,
-                                                   velocity: physicsComponent.velocity,
-                                                   affectedByGravity: physicsComponent.affectedByGravity,
-                                                   linearDamping: physicsComponent.linearDamping,
-                                                   isDynamic: physicsComponent.isDynamic,
-                                                   allowsRotation: physicsComponent.allowsRotation,
-                                                   restitution: physicsComponent.restitution,
-                                                   friction: physicsComponent.friction,
-                                                   categoryBitMask: categoryBitMask,
-                                                   collisionBitMask: collisionBitMask,
-                                                   contactBitMask: contactBitMask)!
-        // TODO: take out the force unwrap above?
+        let physicsBody: PhysicsBody? = PhysicsBody(position: positionComponent.position,
+                                                    size: physicsComponent.size,
+                                                    radius: physicsComponent.radius,
+                                                    zRotation: rotationComponent.angleInRadians,
+                                                    mass: physicsComponent.mass,
+                                                    velocity: physicsComponent.velocity,
+                                                    affectedByGravity: physicsComponent.affectedByGravity,
+                                                    linearDamping: physicsComponent.linearDamping,
+                                                    isDynamic: physicsComponent.isDynamic,
+                                                    allowsRotation: physicsComponent.allowsRotation,
+                                                    restitution: physicsComponent.restitution,
+                                                    friction: physicsComponent.friction,
+                                                    categoryBitMask: categoryBitMask,
+                                                    collisionBitMask: collisionBitMask,
+                                                    contactBitMask: contactBitMask)
         return physicsBody
-    }
-
-    func getPosition(of entityId: String) -> CGPoint? {
-        // MUST be node.
-        scene.getPosition(of: entityId)
-    }
-
-    func getBitmasks(of entityId: String) -> String {
-        "\(scene.getBitMasks(of: entityId))"
     }
 }

35799133b2713aae59468c3daeb21811a444d0e3
xlzior <wenjun.lye@gmail.com>
2023-04-07 00:46:51 +0800

    Make AutoRotate general

--- DumbDuels/DumbDuels/SpaceshipGame/Components/AutoRotateComponent.swift
+++ DumbDuels/DumbDuels/SpaceshipGame/Components/AutoRotateComponent.swift
@@ -5,12 +5,15 @@
 //  Created by Wen Jun Lye on 28/3/23.
 //
 
+import CoreGraphics
 import DuelKit
 
 class AutoRotateComponent: Component {
     var id: ComponentID
+    var anglePerLoop: CGFloat
 
-    init() {
+    init(by anglePerLoop: CGFloat) {
         self.id = ComponentID()
+        self.anglePerLoop = anglePerLoop
     }
 }
--- DumbDuels/DumbDuels/SpaceshipGame/Systems/AutoRotateSystem.swift
+++ DumbDuels/DumbDuels/SpaceshipGame/Systems/AutoRotateSystem.swift
@@ -9,16 +9,16 @@ import DuelKit
 
 class AutoRotateSystem: System {
     unowned var entityManager: EntityManager
-    private var spaceships: Assemblage3<SpaceshipComponent, RotationComponent, AutoRotateComponent>
+    private var objects: Assemblage2<RotationComponent, AutoRotateComponent>
 
     init(for entityManager: EntityManager) {
         self.entityManager = entityManager
-        self.spaceships = entityManager.assemblage(requiredComponents: SpaceshipComponent.self, RotationComponent.self, AutoRotateComponent.self)
+        self.objects = entityManager.assemblage(requiredComponents: RotationComponent.self, AutoRotateComponent.self)
     }
 
     func update() {
-        for (_, rotation, _) in spaceships {
-            rotation.angleInRadians = (rotation.angleInRadians + SPConstants.rotationSpeed)
+        for (rotation, autoRotate) in objects {
+            rotation.angleInRadians = (rotation.angleInRadians + autoRotate.anglePerLoop)
                 .truncatingRemainder(dividingBy: 2 * Double.pi)
         }
     }

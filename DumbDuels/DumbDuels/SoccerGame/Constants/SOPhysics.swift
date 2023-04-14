c724e0fb00b788bec72f4be4c8d1332d61c99e7b
Bryan Kwok <bryankwokyf@gmail.com>
2023-04-14 16:46:58 +0800

    Make soccer game use PhysicsDetails

--- DumbDuels/DumbDuels/SoccerGame/Constants/SOPhysics.swift
+++ DumbDuels/DumbDuels/SoccerGame/Constants/SOPhysics.swift
@@ -9,47 +9,73 @@ import CoreGraphics
 import DuelKit
 
 struct SOPhysics {
-    static let playerMass: CGFloat = 100.0
-    static let playerGravity = false
-    static let playerCor: CGFloat = 0
-    static let playerIsDynamic = true
-    static let playerZRotation = PhysicsEngineDefaults.zRotation
-    static let playerRotation = false
-    static let playerFriction: CGFloat = 0
-    static let playerStaticDamping: CGFloat = 0
-    static let playerImpulse: CGVector = .zero
-    static let playerAngularImpulse: CGFloat = .zero
+    static let playerPhysicsDetails = PhysicsDetails(
+        mass: 100,
+        affectedByGravity: false,
+        linearDamping: 0,
+        allowsRotation: false,
+        restitution: 0,
+        friction: 0,
+        ownBitmask: SOCollisions.playerBitmask,
+        collideBitmask: SOCollisions.playerCollideBitmask,
+        contactBitmask: SOCollisions.playerContactBitmask
+    )
 
-    static let ballMass: CGFloat = 1.0
-    static let ballGravity = false
-    static let ballCor: CGFloat = 0.8
-    static let ballIsDynamic = true
-    static let ballZRotation = PhysicsEngineDefaults.zRotation
-    static let ballRotation = true
-    static let ballFriction: CGFloat = 0
-    static let ballStaticDamping: CGFloat = 0.5
-    static let ballImpulse: CGVector = .zero
-    static let ballAngularImpulse: CGFloat = .zero
+    static let ballPhysicsDetails = PhysicsDetails(
+        affectedByGravity: false,
+        linearDamping: 0.5,
+        restitution: 0.8,
+        friction: 0,
+        ownBitmask: SOCollisions.ballBitmask,
+        collideBitmask: SOCollisions.ballCollideBitmask,
+        contactBitmask: SOCollisions.ballContactBitmask
+    )
 
-    static let goalMass: CGFloat = 1.0
-    static let goalGravity = false
-    static let goalCor: CGFloat = 0.8
-    static let goalIsDynamic = false
-    static let goalZRotation = PhysicsEngineDefaults.zRotation
-    static let goalRotation = false
-    static let goalFriction: CGFloat = 0
-    static let goalStaticDamping: CGFloat = 0
-    static let goalImpulse: CGVector = .zero
-    static let goalAngularImpulse: CGFloat = .zero
+    static let goalPhysicsDetails = PhysicsDetails(
+        affectedByGravity: false,
+        linearDamping: 0,
+        isDynamic: false,
+        allowsRotation: false,
+        restitution: 0.8,
+        friction: 0,
+        ownBitmask: SOCollisions.goalBitmask,
+        collideBitmask: SOCollisions.goalCollideBitmask,
+        contactBitmask: SOCollisions.goalContactBitmask
+    )
 
-    static let wallMass: CGFloat = 1.0
-    static let wallGravity = false
-    static let wallCor: CGFloat = 0.8
-    static let wallIsDynamic = false
-    static let wallZRotation = PhysicsEngineDefaults.zRotation
-    static let wallRotation = false
-    static let wallFriction: CGFloat = 0
-    static let wallStaticDamping: CGFloat = 0
-    static let wallImpulse: CGVector = .zero
-    static let wallAngularImpulse: CGFloat = .zero
+    static let goalSidePhysicsDetails = PhysicsDetails(
+        affectedByGravity: false,
+        linearDamping: 0,
+        isDynamic: false,
+        allowsRotation: false,
+        restitution: 0.8,
+        friction: 0,
+        ownBitmask: SOCollisions.goalSideBitmask,
+        collideBitmask: SOCollisions.goalSideCollideBitmask,
+        contactBitmask: SOCollisions.goalSideContactBitmask
+    )
+
+    static let goalBackPhysicsDetails = PhysicsDetails(
+        affectedByGravity: false,
+        linearDamping: 0,
+        isDynamic: false,
+        allowsRotation: false,
+        restitution: 0.8,
+        friction: 0,
+        ownBitmask: SOCollisions.goalBackBitmask,
+        collideBitmask: SOCollisions.goalBackCollideBitmask,
+        contactBitmask: SOCollisions.goalBackContactBitmask
+    )
+
+    static let wallPhysicsDetails = PhysicsDetails(
+        affectedByGravity: false,
+        linearDamping: 0,
+        isDynamic: false,
+        allowsRotation: false,
+        restitution: 1,
+        friction: 0,
+        ownBitmask: SOCollisions.wallBitmask,
+        collideBitmask: SOCollisions.wallContactBitmask,
+        contactBitmask: SOCollisions.wallCollideBitmask
+    )
 }
